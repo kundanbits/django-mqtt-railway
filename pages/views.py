0eeb82d59e6db4fa79fdbba827fcdfccbf9846ab
kundanbits <kundanju@gmail.com>
2026-02-23 02:12:18 +0530

    fix missing home view

--- pages/views.py
+++ pages/views.py
@@ -1,19 +1,32 @@
-import json
+from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import Accelerometer
+import json
+
+
+def home(request):
+    return render(request, "pages/home.html")
+
+
+def sensor_data(request):
+    last_id = request.GET.get("last_id")
+
+    if last_id:
+        data = Accelerometer.objects.filter(id__gt=last_id).order_by("id")
+    else:
+        data = Accelerometer.objects.order_by("-id")[:50]
+        data = reversed(data)
+
+    data = list(data.values("x", "y", "z", "id"))
+    return JsonResponse(data, safe=False)
 
 
 @csrf_exempt
 def save_sensor_data(request):
     if request.method == "POST":
         try:
-            raw_body = request.body.decode("utf-8")
-            print("RAW BODY:", raw_body)
-
-            data = json.loads(raw_body)
-
-            print("PARSED DATA:", data)
+            data = json.loads(request.body)
 
             x = float(data.get("x", 0))
             y = float(data.get("y", 0))
